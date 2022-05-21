5889f50f4d831a0a1e8d994ef640b62e1f58791c
wrac4242 <wrac4242@gmail.com>
2022-05-21 22:54:56 +0100

    added camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::window::PresentMode;
+
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
 fn main() {
-    println!("Hello, world!");
+    let height = 900.0;
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height: height,
+            title: "Physics Simulator".to_string(),
+            resizable: false,
+            decorations: false,
+            present_mode: PresentMode::Mailbox,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+
+    camera.orthographic_projection.left = -1.0 * RESOLUTION;
+    camera.orthographic_projection.right = 1.0 * RESOLUTION;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
 }
