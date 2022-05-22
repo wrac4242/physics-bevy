779fe87e0b1db0819242eab0c91d2d76ddab42de
wrac4242 <wrac4242@gmail.com>
2022-05-22 16:43:26 +0100

    setup for physics update

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,15 @@ fn main() {
         // own systems
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_ball)
+
+        // .add_system(apply_gravity)
+        .add_system_set(
+            SystemSet::new()
+                .label("apply_physics")
+                .with_system(apply_gravity)
+        )
+
+        .add_system(update_positions.after("apply_physics"))
         
         // debugging
         .add_system(bevy::input::system::exit_on_esc_system)
@@ -66,4 +75,12 @@ fn spawn_ball(mut commands: Commands, asset_server: Res<AssetServer>) {
         acceleration: Vec2::new(0., 0.),
         radius: radius,
     });
+}
+
+fn apply_gravity() {
+    todo!();
+}
+
+fn update_positions() {
+    todo!();
 }
